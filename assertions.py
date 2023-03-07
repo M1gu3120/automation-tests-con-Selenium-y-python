8db71b5837375002d7e0f39bb0e3b09f140c3f43
M1gu3120 <112451633+M1gu3120@users.noreply.github.com>
2023-03-07 00:45:13 -0500

    Update assertions.py

--- assertions.py
+++ assertions.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 
 class AssertionsTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = r"C:\Users\Ing Miguel Angel\Desktop\chromedriver.exe")
-        driver = self.driver
+        self.driver = webdriver.Chrome(executable_path = r"C:\chromedriver.exe") // poner la ruta donde se tiene alojado el chromedrive.exe recomiendo pegar
+        driver = self.driver                                                     en la raiz de C//
         driver.implicitly_wait(30)
         driver.maximize_window()
-        driver.get('http://demo-store.seleniumacademy.com/')
+        driver.get('http://demo-store.seleniumacademy.com/')             //Poner el link de la pagina que le va hacer pruebas 
 
     def test_search_field(self):
         self.assertTrue(self.is_element_present(By.NAME, 'q'))
